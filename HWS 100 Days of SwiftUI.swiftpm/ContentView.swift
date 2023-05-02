6db3c4a1f38d7e80c1629f7133ddbc1de2a51efb
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-05-02 09:39:33 -0400

    Day 6: FizzBuzz

--- HWS 100 Days of SwiftUI.swiftpm/ContentView.swift
+++ HWS 100 Days of SwiftUI.swiftpm/ContentView.swift
@@ -1,8 +1,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var messages: [String]
+    
+    init() {
+        self.messages = [String]()
+        for i in 1...100 {
+            if i % 15 == 0 {
+                messages.append("FizzBuzz")
+            } else if i % 3 == 0 {
+                messages.append("Fizz")
+            } else if i % 5 == 0 {
+                messages.append("Buzz")
+            } else {
+                messages.append(String(i))
+            }
+        }
+    }
    
     var body: some View {
-        Text("Hello world")
+         ScrollView {
+            VStack {
+                ForEach(messages, id: \.self) { entry in
+                    Text(entry)
+                }
+            }
+        }
     }
 }
