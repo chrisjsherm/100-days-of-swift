fb63a21435527e95e0a98778fe9c8e804cb13488
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-05-05 10:31:29 -0400

    Day 9: Checkpoint

--- HWS 100 Days of SwiftUI.swiftpm/ContentView.swift
+++ HWS 100 Days of SwiftUI.swiftpm/ContentView.swift
@@ -2,9 +2,23 @@ import SwiftUI
 
 struct ContentView: View {
     @State var logEntry: [String]
+    let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
     
     init() {
         self.logEntry = [String]()
+        
+        luckyNumbers.filter({(val: Int) -> Bool in
+            return val % 2 != 0
+        })
+        .sorted(by: {(val1: Int, val2: Int) -> Bool in
+            return val1 < val2
+        })
+        .map({(val: Int) -> String in
+            return "\(val) is a lucky number"
+        })
+        .forEach({ (val: String) -> Void in
+            print(val)
+        })
     }
    
     var body: some View {
