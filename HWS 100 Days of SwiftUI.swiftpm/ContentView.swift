e349a303b5bd2b7b23c877bac00fc67a1b64eeb3
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-05-13 09:34:01 -0400

    Day 14/Checkpoint 9Optionals

--- HWS 100 Days of SwiftUI.swiftpm/ContentView.swift
+++ HWS 100 Days of SwiftUI.swiftpm/ContentView.swift
@@ -1,18 +1,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    let building1: Building
-    let building2: Building
     
     init() {
-        building1 = House(rooms: 3, cost: 250000, agentName: "Bill")
-        building2 = Office(rooms: 50, cost: 1000000, agentName: "Lucy")
+        
+        func hello(arr: [Int]?) -> Int { return arr?[Int.random(in: 0..<(arr!.count))] ?? Int.random(in: 1...100)}
+        print(hello(arr: nil))
     }
     
     var body: some View {
-        VStack{
-            Text("Building 1 summary: \(building1.printSalesSummary())")
-            Text("Building 2 summary: \(building2.printSalesSummary())")
-        }
+        Text("Checkpoint 9")
     }
 }
