428d64341ba387ad0ec2c6c858cffe113246fb45
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-05-25 09:35:49 -0400

    Day 23: Custom modifier with view extension--watermarked

--- Views and Modifiers/Views and Modifiers/ContentView.swift
+++ Views and Modifiers/Views and Modifiers/ContentView.swift
@@ -9,10 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            CapsuleText(text: "Hello")
-            CapsuleText(text: "World")
-        }
+        Color.blue
+            .frame(width: 300, height: 200)
+            .watermarked(with: "Hacking with Swift")
     }
 }
 
---
+++ Views and Modifiers/Watermark.swift
@@ -0,0 +1,29 @@
+//
+//  Watermark.swift
+//  Views and Modifiers
+//
+//  Created by Christopher Sherman on 2023-05-25.
+//
+
+import SwiftUI
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
