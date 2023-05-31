76bedce8adefda7885f4eedc93a9dc8fdb082bbb
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-05-31 10:06:58 -0400

    Day 26: DatePicker

--- better-rest/better-rest/ContentView.swift
+++ better-rest/better-rest/ContentView.swift
@@ -8,19 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount: Double
-    
-    init() {
-        sleepAmount = 8.0
-    }
-    
+    @State private var sleepAmount = 8.0
+    @State private var wakeDate = Date.now
+        
     var body: some View {
-        VStack {
+        NavigationStack {
             VStack {
-                Stepper("\(sleepAmount.formatted()) \(sleepAmount.formatted() == "1" ? "hour" : "hours")", value: $sleepAmount, in: 0...24, step: 0.25)
+                VStack {
+                    DatePicker("Please enter a date", selection: $wakeDate)
+                    
+                    Stepper("\(sleepAmount.formatted()) \(sleepAmount.formatted() == "1" ? "hour" : "hours")", value: $sleepAmount, in: 0...24, step: 0.25)
+                }
             }
+            .padding()
         }
-        .padding()
+        .navigationTitle("Better Rest")
     }
 }
 
