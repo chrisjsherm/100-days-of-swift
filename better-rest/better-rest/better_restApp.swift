19b01d5fc33f28ca71a428907bfdbdff28ea62e0
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-05-31 09:49:07 -0400

    Day 26: BetterRest app boilerplate

---
+++ better-rest/better-rest/better_restApp.swift
@@ -0,0 +1,17 @@
+//
+//  better_restApp.swift
+//  better-rest
+//
+//  Created by Christopher Sherman on 2023-05-31.
+//
+
+import SwiftUI
+
+@main
+struct better_restApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
