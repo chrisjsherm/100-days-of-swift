d29c233d31dcc9ea28e165740ed204a1cb52de54
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-06-06 08:52:20 -0400

    Day 30: WordScrable initial app boilerplate

---
+++ Word Scramble/Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Christopher Sherman on 2023-06-06.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
