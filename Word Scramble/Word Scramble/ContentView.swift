36c409330a6c511a519a2361bf125c5fd747c635
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-06-06 09:01:25 -0400

    Day 30: Word entry

--- Word Scramble/Word Scramble/ContentView.swift
+++ Word Scramble/Word Scramble/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard answer.count > 0 else {return}
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
         }
-        .padding()
+        newWord = ""
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+        }.navigationTitle(rootWord)
+            .onSubmit(addNewWord)
     }
 }
 
