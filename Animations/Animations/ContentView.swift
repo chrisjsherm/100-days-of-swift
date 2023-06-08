aabefd58fca49ec51672e6f5bdff5db7dd690132
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-06-08 09:14:04 -0400

    Day 32: 3D rotate button on tap

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,31 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 1.0
+    @State private var animationAmount = 0.0
     
     var body: some View {
-        VStack {
+        return VStack {
             Button("Tap Me") {
-                // animationAmount += 1
+                withAnimation(.interpolatingSpring(stiffness: 10, damping: 5)) {
+                    animationAmount += 360
+                }
             }
-            .padding(50)
+            .padding(40)
             .background(.red)
             .foregroundColor(.white)
             .clipShape(Circle())
-            .overlay(
-                Circle()
-                    .stroke(.red)
-                    .scaleEffect(animationAmount)
-                    .opacity(2 - animationAmount)
-                    .animation(
-                        .easeInOut(duration: 1)
-                            .repeatForever(autoreverses: false),
-                        value: animationAmount
-                    )
-            )
-            .onAppear {
-                animationAmount = 2
-            }
+            .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 1))
         }
         .padding()
     }
