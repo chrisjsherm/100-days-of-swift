521752a1678512d063d9d75648b5568763448a4a
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-06-08 08:46:24 -0400

    Day 32: Animations app boilerplate

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Christopher Sherman on 2023-06-08.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
