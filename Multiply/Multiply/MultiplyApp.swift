24122f3c829d0884521b3f6c14e54512313e2f60
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-06-15 09:15:07 -0400

    Day 35: Multiply app boilerplate

---
+++ Multiply/Multiply/MultiplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplyApp.swift
+//  Multiply
+//
+//  Created by Christopher Sherman on 2023-06-15.
+//
+
+import SwiftUI
+
+@main
+struct MultiplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
