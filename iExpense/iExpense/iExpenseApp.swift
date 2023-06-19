4843444712842cac869d39654060e2a64f5573dc
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-06-19 09:30:19 -0400

    Day 36: iExpense app boilerplate

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Christopher Sherman on 2023-06-19.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
