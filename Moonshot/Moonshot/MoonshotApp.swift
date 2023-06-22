c2d63dd7415f6fac55b2ed675a38acad1ace8e12
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-06-22 08:44:51 -0400

    Moonshot: app boilerplate

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Christopher Sherman on 2023-06-22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
