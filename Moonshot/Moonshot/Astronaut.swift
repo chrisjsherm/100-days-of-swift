6e2f7770c7f850d8c2cd1595acdecd9d547dffe2
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-06-23 09:06:58 -0400

    Moonshot: Bundle extension to load JSON file into struct

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronat.swift
+//  Moonshot
+//
+//  Created by Christopher Sherman on 2023-06-23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
