0b0046b29a31121754696483cedb6d9a34a7f661
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-06-28 08:37:52 -0400

    Day 43: Insettable shape so border gets stroked inside the boundary

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,10 +7,11 @@
 
 import SwiftUI
 
-struct Arc: Shape {
+struct Arc: InsettableShape {
     var startAngle: Angle
     var endAngle: Angle
     var clockwise: Bool
+    var insetAmount = 0.0
 
     func path(in rect: CGRect) -> Path {
         let rotationAdjustment = Angle.degrees(90)
@@ -18,20 +19,22 @@ struct Arc: Shape {
         let modifiedEnd = endAngle - rotationAdjustment
         
         var path = Path()
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
 
         return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
-                .stroke(.blue, lineWidth: 10)
-                .frame(width: 300, height: 300)
-        }
-        .padding()
+        Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+            .strokeBorder(.blue, lineWidth: 10)
     }
 }
 
