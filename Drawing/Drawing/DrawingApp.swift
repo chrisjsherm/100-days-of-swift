4d5be9d87ef0c0cd7f2eb3424268eeeee914dcd5
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-06-28 08:15:36 -0400

    Day 43/Drawing boilerplate

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Christopher Sherman on 2023-06-28.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
