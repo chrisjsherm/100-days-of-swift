fc1cecee4f34db1b15e6450bbfc8a6b33a609791
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-06-28 08:23:47 -0400

    Day 43: Create a Shape struct

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,16 +7,25 @@
 
 import SwiftUI
 
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+
+        return path
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Path { path in
-                path.move(to: CGPoint(x: 200, y: 100))
-                path.addLine(to: CGPoint(x: 100, y: 300))
-                path.addLine(to: CGPoint(x: 300, y: 300))
-                path.addLine(to: CGPoint(x: 200, y: 100))
-            }
-            .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+            Triangle()
+                .fill(.red)
+                .frame(width: 300, height: 300)
         }
         .padding()
     }
