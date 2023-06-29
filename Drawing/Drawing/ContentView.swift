8fa6a6049c5c7d51ac6590a417c6f057e9bb610c
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-06-29 09:20:53 -0400

    Day 44: Color cycling circle

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var petalOffset = -14.0
-    @State private var petalWidth = 51.0
-    
+    @State private var colorCycle = 0.0
+
     var body: some View {
         VStack {
-            Text("Hello World")
+            ColorCyclingCircle(amount: colorCycle)
                 .frame(width: 300, height: 300)
-                .border(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.3), width: 30)
+
+            Slider(value: $colorCycle)
         }
     }
 }
