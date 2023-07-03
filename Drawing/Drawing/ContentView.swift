a2948bb55424b1a065f1920daa889593e34b8394
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-07-03 11:09:47 -0400

    Day 46: Adjust arrow stroke width with Slider

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var strokeWidth = 5.0
 
     var body: some View {
         VStack {
             Text("Arrow")
             
             Arrow()
-                .stroke(.blue, style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round))
+                .stroke(.blue, style: StrokeStyle(lineWidth: strokeWidth, lineCap: .round, lineJoin: .round))
                 .frame(width: 200, height: 200)
+            
+            Slider(value: $strokeWidth, in: 1.0...25.0)
+                .padding()
         }
     }
 }
