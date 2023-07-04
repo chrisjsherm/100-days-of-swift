d291b4aeb04195bddcce37aa6e74d2bb04208346
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-07-04 13:52:06 -0400

    Day 47: Habits boilerplate

---
+++ Habits/Habits/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Habits
+//
+//  Created by Christopher Sherman on 2023-07-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Habits/Habits/HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by Christopher Sherman on 2023-07-04.
+//
+
+import SwiftUI
+
+@main
+struct HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
