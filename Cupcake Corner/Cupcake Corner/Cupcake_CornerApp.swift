be825de098e87b95e4307b3e3a1cf253b24bd639
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-07-07 09:31:13 -0400

    Day 49: Cupcake corner boilerplate

---
+++ Cupcake Corner/Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Christopher Sherman on 2023-07-07.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
