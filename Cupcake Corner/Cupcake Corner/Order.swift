277580b06f09ae475adf2e7823ba5a86921e9173
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-07-10 09:35:15 -0400

    Cupcake Corner: Order class and basic UI

---
+++ Cupcake Corner/Cupcake Corner/Order.swift
@@ -0,0 +1,26 @@
+//
+//  Order.swift
+//  Cupcake Corner
+//
+//  Created by Christopher Sherman on 2023-07-10.
+//
+
+import Foundation
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+
+    @Published var type = 0
+    @Published var quantity = 3
+
+    @Published var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+}
