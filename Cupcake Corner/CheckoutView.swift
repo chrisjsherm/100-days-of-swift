bcd3467970e4d04a95913e0eb849a666b104ca6d
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-07-10 09:41:43 -0400

    Cupcake Corner: Address view

---
+++ Cupcake Corner/CheckoutView.swift
@@ -0,0 +1,22 @@
+//
+//  CheckoutView.swift
+//  Cupcake Corner
+//
+//  Created by Christopher Sherman on 2023-07-10.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @ObservedObject var order: Order
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView(order: Order())
+    }
+}
