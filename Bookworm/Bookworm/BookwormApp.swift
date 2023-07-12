0c76e22c479870eed8a55fd64cbc58b7ecaab621
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-07-12 09:16:52 -0400

    Day 53: Bookworm app boilerplate

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Christopher Sherman on 2023-07-12.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
