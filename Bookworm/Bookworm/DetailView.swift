ac489b903154541c96a60a5a446ad91d4e8e5412
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-07-13 09:02:54 -0400

    Bookworm: DetailView

--- Bookworm/Bookworm/DetailView.swift
+++ Bookworm/Bookworm/DetailView.swift
@@ -12,8 +12,34 @@ struct DetailView: View {
     let book: Book
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre ?? "Fantasy")
+                    .resizable()
+                    .scaledToFit()
+
+                Text(book.genre?.uppercased() ?? "FANTASY")
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundColor(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(Capsule())
+                    .offset(x: -5, y: -5)
+            }
+            
+            Text(book.author ?? "Unknown author")
+                .font(.title)
+                .foregroundColor(.secondary)
+
+            Text((book.review ?? "")?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ? "No review" : book.review!)
+                .padding()
+
+            RatingView(rating: .constant(Int(book.rating)))
+                .font(.largeTitle)
+        }
+        .navigationTitle(book.title ?? "Unknown Book")
+        .navigationBarTitleDisplayMode(.inline)    }
 }
 
 struct DetailView_Previews: PreviewProvider {
