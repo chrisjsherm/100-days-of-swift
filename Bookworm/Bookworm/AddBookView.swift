054d0ee3064e3240816fde9d77aa8b3f5ef46912
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-07-13 08:07:43 -0400

    Day 54/Bookworm: Add Book form

---
+++ Bookworm/Bookworm/AddBookView.swift
@@ -0,0 +1,68 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Christopher Sherman on 2023-07-13.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    @Environment(\.managedObjectContext) var context
+    @State private var title = ""
+    @State private var author = ""
+    @State private var rating = 3
+    @State private var genre = ""
+    @State private var review = ""
+    
+    private let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Name of book", text: $title)
+                    TextField("Author's name", text: $author)
+
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+
+                Section {
+                    TextEditor(text: $review)
+
+                    Picker("Rating", selection: $rating) {
+                        ForEach(0..<6) {
+                            Text(String($0))
+                        }
+                    }
+                } header: {
+                    Text("Write a review")
+                }
+
+                Section {
+                    Button("Save") {
+                        let newBook = Book(context: context)
+                        newBook.id = UUID()
+                        newBook.title = title
+                        newBook.author = author
+                        newBook.rating = Int16(rating)
+                        newBook.genre = genre
+                        newBook.review = review
+                        
+                        try? context.save()
+                    }
+                }
+            }
+            .navigationTitle("Add Book")
+        }
+    }
+}
+
+struct AddBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddBookView()
+    }
+}
