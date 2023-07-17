3d4243a4a784b3cf7ba2b33071fc41bceee98072
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-07-17 09:21:47 -0400

    Bookworm: Add validation for author and title

--- Bookworm/Bookworm/AddBookView.swift
+++ Bookworm/Bookworm/AddBookView.swift
@@ -16,15 +16,21 @@ struct AddBookView: View {
     @State private var genre = ""
     @State private var review = ""
     @State private var reviewPlaceholder = "What did you think?"
+    @State private var isSubmitClicked = false
     
     private let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+    private var isFormDisabled: Bool {
+        title.count == 0 || author.count == 0
+    }
+    
+    
     var body: some View {
         NavigationView {
             Form {
                 Section {
                     TextField("Name of book", text: $title)
                     TextField("Author's name", text: $author)
-
+                    
                     Picker("Genre", selection: $genre) {
                         ForEach(genres, id: \.self) {
                             Text($0)
@@ -52,6 +58,11 @@ struct AddBookView: View {
 
                 Section {
                     Button("Save") {
+                        if isFormDisabled {
+                            isSubmitClicked = true
+                            return
+                        }
+                        
                         let newBook = Book(context: context)
                         newBook.id = UUID()
                         newBook.title = title
@@ -63,6 +74,21 @@ struct AddBookView: View {
                         try? context.save()
                         dismiss()
                     }
+                    .disabled(isSubmitClicked && isFormDisabled)
+                    
+                    if isSubmitClicked && isFormDisabled {
+                        if title.count == 0 {
+                            Text("Title is a required field")
+                                .font(.caption)
+                                .foregroundColor(.red)
+                        }
+                        if author.count == 0 {
+                            Text("Author is a required field")
+                                .font(.caption)
+                                .foregroundColor(.red)
+                        }
+                    }
+                        
                 }
             }
             .navigationTitle("Add Book")
