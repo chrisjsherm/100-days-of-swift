816cfc99843e85ed9f2232a4614b61f4a227886b
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-07-18 08:30:41 -0400

    Day 57: Core Data Project boilerplate

---
+++ CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Christopher Sherman on 2023-07-18.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
