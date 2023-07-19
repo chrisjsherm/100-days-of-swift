cb361a670f31b59154d207e92d695c0ac4bd10ea
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-07-19 09:00:41 -0400

    Day 58: Make FilteredList view generic

--- CoreDataProject/CoreDataProject/ContentView.swift
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -14,8 +14,12 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            FilteredList(filter: lastNameFilter)
-
+            FilteredList(
+                filterKey: "lastName", filterValue: lastNameFilter
+            ) { (singer: Singer) in
+                Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+            }
+            
             Button("Add Examples") {
                 let taylor = Singer(context: moc)
                 taylor.firstName = "Taylor"
--- CoreDataProject/CoreDataProject/FilteredList.swift
+++ CoreDataProject/CoreDataProject/FilteredList.swift
@@ -5,18 +5,30 @@
 //  Created by Christopher Sherman on 2023-07-19.
 //
 
+import CoreData
 import SwiftUI
 
-struct FilteredList: View {
-    @FetchRequest var fetchRequest: FetchedResults<Singer>
-    
-    init(filter: String) {
-        _fetchRequest = FetchRequest<Singer>(sortDescriptors: [], predicate: NSPredicate(format: "lastName BEGINSWITH %@", filter))
-    }
+struct FilteredList<T: NSManagedObject, Content: View>: View {
+    @FetchRequest var fetchRequest: FetchedResults<T>
+
+    // this is our content closure; we'll call this once for each item in the list
+    let content: (T) -> Content
+
     var body: some View {
         List(fetchRequest, id: \.self) { singer in
-            Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+            self.content(singer)
         }
     }
-}
 
+    init(
+        filterKey: String,
+        filterValue: String,
+        @ViewBuilder content: @escaping (T) -> Content
+    ) {
+        _fetchRequest = FetchRequest<T>(
+            sortDescriptors: [],
+            predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue)
+        )
+        self.content = content
+    }
+}
