6d8d858e9058b3d8a8cdfb9ca74991c5ceb96c89
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-07-21 08:48:20 -0400

    FriendInfo boilerplate

---
+++ FriendInfo/FriendInfo/FriendInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendInfoApp.swift
+//  FriendInfo
+//
+//  Created by Christopher Sherman on 2023-07-21.
+//
+
+import SwiftUI
+
+@main
+struct FriendInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
