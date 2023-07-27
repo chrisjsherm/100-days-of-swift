111460e036d7f3484c70ad3b6a9d9f4e6e41177f
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-07-27 08:18:30 -0400

    Day 62/Instafilter: Boilerplate

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Christopher Sherman on 2023-07-27.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
