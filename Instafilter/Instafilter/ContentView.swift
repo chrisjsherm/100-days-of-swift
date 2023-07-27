8c21625470a99f618fd4cef93b45484cd170cbd9
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-07-27 08:47:05 -0400

    Day 63: Conditionally adjust Core Image filter based on whether the property exists for a given filter

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -26,9 +26,18 @@ struct ContentView: View {
         let beginImage = CIImage(image: inputImage)
 
         let context = CIContext()
-        let currentFilter = CIFilter.sepiaTone()
+        
+        let currentFilter = CIFilter.twirlDistortion()
         currentFilter.inputImage = beginImage
-        currentFilter.intensity = 1
+
+        let amount = 1.0
+
+        let inputKeys = currentFilter.inputKeys
+
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey) }
         
         // get a CIImage from our filter or exit if that fails
         guard let outputImage = currentFilter.outputImage else { return }
