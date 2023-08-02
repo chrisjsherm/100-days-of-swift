0240a06c9a04a7671885e66131a425a4059fb698
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-08-02 08:42:14 -0400

    Day 66/Instafilter: Change filter selection with confirmation dialog

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -14,7 +14,8 @@ struct ContentView: View {
     @State private var showingImagePicker = false
     @State private var inputImage: UIImage?
     @State private var filterIntensity = 0.5
-    @State private var currentFilter = CIFilter.sepiaTone()
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
+    @State private var showingFilterSheet = false
     
     let context = CIContext()
 
@@ -32,6 +33,11 @@ struct ContentView: View {
                             filterImage()
                         }
                     Text("Filter intensity: \(filterIntensity, specifier: "%.1f")")
+                    
+                    Button("Change filter") {
+                        showingFilterSheet = true
+                    }
+                    .padding(.top)
                     
                     Button("Save photo", action: save)
                         .padding(.top)
@@ -54,6 +60,16 @@ struct ContentView: View {
             .sheet(isPresented: $showingImagePicker) {
                 ImagePicker(image: $inputImage)
             }
+            .confirmationDialog("Select a filter", isPresented: $showingFilterSheet) {
+                Button("Crystallize") { setFilter(CIFilter.crystallize()) }
+                Button("Edges") { setFilter(CIFilter.edges()) }
+                Button("Gaussian Blur") { setFilter(CIFilter.gaussianBlur()) }
+                Button("Pixellate") { setFilter(CIFilter.pixellate()) }
+                Button("Sepia Tone") { setFilter(CIFilter.sepiaTone()) }
+                Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask()) }
+                Button("Vignette") { setFilter(CIFilter.vignette()) }
+                Button("Cancel", role: .cancel) { }
+            }
             .onChange(of: inputImage) {
                 _ in loadImage()
             }
@@ -70,7 +86,12 @@ struct ContentView: View {
     }
     
     func filterImage() {
-        currentFilter.intensity = Float(filterIntensity)
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
+
         guard let outputImage = currentFilter.outputImage else {return}
         
         if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
@@ -84,6 +105,11 @@ struct ContentView: View {
         
         let imageSaver = ImageSaver()
         imageSaver.writeToPhotoAlbum(image: inputImage)
+    }
+    
+    func setFilter(_ filter: CIFilter) {
+        currentFilter = filter
+        loadImage()
     }
 }
 
