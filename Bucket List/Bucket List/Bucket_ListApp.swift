c131b72485d4f21ba654ef4cc7a4e142f982febb
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-08-04 08:36:21 -0400

    Day 68: Bucket List boilerplate

---
+++ Bucket List/Bucket List/Bucket_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_ListApp.swift
+//  Bucket List
+//
+//  Created by Christopher Sherman on 2023-08-04.
+//
+
+import SwiftUI
+
+@main
+struct Bucket_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
