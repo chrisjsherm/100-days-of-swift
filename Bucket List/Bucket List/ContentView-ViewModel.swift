014e4879cd6ae2bfbd8d4f7de248d2cc84a90932
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-08-10 10:35:17 -0400

    Day 72: Require FaceID in app to unlock app data

--- Bucket List/Bucket List/ContentView-ViewModel.swift
+++ Bucket List/Bucket List/ContentView-ViewModel.swift
@@ -7,12 +7,14 @@
 
 import Foundation
 import MapKit
+import LocalAuthentication
 
 extension ContentView {
     @MainActor class ViewModel: ObservableObject {
         @Published var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
         @Published private(set) var locations: Array<Location>
         @Published var selectedPlace: Location?
+        @Published var isUnlocked = false
         
         let savePath = FileManager.documentsDirectory.appendingPathComponent("SavedPlaces")
         
@@ -31,12 +33,25 @@ extension ContentView {
             save()
         }
         
-        func update(location: Location) {
-            guard let selectedPlace = selectedPlace else { return }
+        func authenticate() {
+            let context = LAContext()
+            var error: NSError?
 
-            if let index = locations.firstIndex(of: selectedPlace) {
-                locations[index] = location
-                save()
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                let reason = "Authenticate yourself to view your places"
+
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+
+                    if success {
+                        Task { @MainActor in
+                            self.isUnlocked = true
+                        }
+                    } else {
+                        // error
+                    }
+                }
+            } else {
+                // no biometrics
             }
         }
         
@@ -46,6 +61,15 @@ extension ContentView {
                 try data.write(to: savePath, options: [.atomic, .completeFileProtection])
             } catch {
                 print("Unable to save data.")
+            }
+        }
+        
+        func update(location: Location) {
+            guard let selectedPlace = selectedPlace else { return }
+
+            if let index = locations.firstIndex(of: selectedPlace) {
+                locations[index] = location
+                save()
             }
         }
     }
