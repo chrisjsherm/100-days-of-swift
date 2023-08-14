0c04894a9486d8d87cee1b51050371d3c7848eac
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-08-14 09:55:50 -0400

    Day 74: Voiceover app boilerplate

---
+++ Voice Over/Voice Over/Voice_OverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Voice_OverApp.swift
+//  Voice Over
+//
+//  Created by Christopher Sherman on 2023-08-14.
+//
+
+import SwiftUI
+
+@main
+struct Voice_OverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
