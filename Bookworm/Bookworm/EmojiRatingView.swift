09c749f54f97c79e6f80623ff7bca191e7ac9183
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-08-15 09:48:15 -0400

    Day 75/Bookworm: Add accessibility label to rating view

--- Bookworm/Bookworm/EmojiRatingView.swift
+++ Bookworm/Bookworm/EmojiRatingView.swift
@@ -11,18 +11,21 @@ struct EmojiRatingView: View {
     let rating: Int16
     
     var body: some View {
-        switch rating {
-        case 1:
-            Text("1")
-        case 2:
-            Text("2")
-        case 3:
-            Text("3")
-        case 4:
-            Text("4")
-        default:
-            Text("5")
+        Group {
+            switch rating {
+            case 1:
+                Text("1")
+            case 2:
+                Text("2")
+            case 3:
+                Text("3")
+            case 4:
+                Text("4")
+            default:
+                Text("5")
+            }
         }
+        .accessibilityLabel("\(rating == 1 ? "1 star" : "\(rating) stars")")
     }
 }
 
