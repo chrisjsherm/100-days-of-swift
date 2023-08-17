71a5e1c0d33f6de27af3a7105c77853eb40fd878
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-08-17 15:14:46 -0400

    Day 77/Rolodex: App boilerplate

---
+++ Rolodex/Rolodex/RolodexApp.swift
@@ -0,0 +1,17 @@
+//
+//  RolodexApp.swift
+//  Rolodex
+//
+//  Created by Christopher Sherman on 2023-08-17.
+//
+
+import SwiftUI
+
+@main
+struct RolodexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
