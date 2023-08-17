6ea53d62e9f3bca33ef5f44c5b4b7240d9c315a7
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-08-17 15:41:31 -0400

    Rolodex: Select profile photo

--- Rolodex/Rolodex/AddContactView.swift
+++ Rolodex/Rolodex/AddContactView.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct AddContactView: View {
+    @State private var showImagePicker = false
+    
+    @State private var image: Image?
+    @State private var inputImage: UIImage?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                if image != nil {
+                    image!
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    Text("Tap to select a profile photo")
+                        .frame(
+                            maxWidth: .infinity,
+                            maxHeight: .infinity
+                        )
+                        .background(Rectangle()
+                            .fill(.white)
+                        )
+                        .foregroundColor(
+                            .primary
+                        )
+                        .onTapGesture {
+                            showImagePicker = true
+                        }
+                }
+            }
+            .sheet(isPresented: $showImagePicker) {
+                ImagePicker(image: $inputImage)
+            }
+            .onChange(of: inputImage) { _ in
+                loadImage()
+            }
+            .navigationTitle("Add Contact")
+        }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        
+        image = Image(uiImage: inputImage)
     }
 }
 
