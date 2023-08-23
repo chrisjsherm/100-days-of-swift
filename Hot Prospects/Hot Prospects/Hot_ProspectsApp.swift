99551747cf73c8d327715c5d5861c7f9420fe3cd
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-08-23 09:57:16 -0400

    Day 79/Hot Prospects: Boilerplate

---
+++ Hot Prospects/Hot Prospects/Hot_ProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hot_ProspectsApp.swift
+//  Hot Prospects
+//
+//  Created by Christopher Sherman on 2023-08-23.
+//
+
+import SwiftUI
+
+@main
+struct Hot_ProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
