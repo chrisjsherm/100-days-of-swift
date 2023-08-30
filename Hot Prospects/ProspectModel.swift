797a85994f30ebb416101da2256ea46d3f106e6e
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-08-30 10:39:02 -0400

    Day 84/Hot Prospects: Save data

--- Hot Prospects/ProspectModel.swift
+++ Hot Prospects/ProspectModel.swift
@@ -15,14 +15,35 @@ class Prospect: Identifiable, Codable {
 }
 
 @MainActor class Prospects: ObservableObject {
-    @Published var people: [Prospect]
+    @Published private(set) var people: [Prospect]
+    
+    private let USER_DEFAULTS_KEY = "SavedData"
 
     init() {
+        if let data = UserDefaults.standard.data(forKey: USER_DEFAULTS_KEY) {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                people = decoded
+                return
+            }
+        }
+        
         self.people = []
+    }
+    
+    func add(_ prospect: Prospect) {
+        people.append(prospect)
+        save()
     }
     
     func toggleIsContacted(_ prospect: Prospect) {
         objectWillChange.send()
         prospect.isContacted.toggle()
+        save()
+    }
+    
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(people) {
+            UserDefaults.standard.set(encoded, forKey: USER_DEFAULTS_KEY)
+        }
     }
 }
