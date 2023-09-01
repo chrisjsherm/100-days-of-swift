8c527b418919aacbca1c03a5960a3e4a170938af
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-09-01 10:57:19 -0400

    Day 86/Flashzilla: Custom haptic

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CoreHaptics
 
 struct ContentView: View {
     // how far the circle has been dragged
@@ -13,6 +14,8 @@ struct ContentView: View {
 
     // whether it is currently being dragged or not
     @State private var isDragging = false
+    
+    @State private var hapticEngine: CHHapticEngine?
 
     var body: some View {
         // a drag gesture that updates offset and isDragging as it moves around
@@ -31,6 +34,8 @@ struct ContentView: View {
                 withAnimation {
                     isDragging = true
                 }
+                
+                onComplexSuccessGesture()
             }
 
         // a combined gesture that forces the user to long press then drag
@@ -43,6 +48,44 @@ struct ContentView: View {
             .scaleEffect(isDragging ? 1.5 : 1)
             .offset(offset)
             .gesture(combined)
+            .onAppear(perform: prepareHaptics)
+    }
+    
+    func onComplexSuccessGesture() {
+        // make sure that the device supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+
+        // create one intense, sharp tap
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try hapticEngine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+
+        do {
+            hapticEngine = try CHHapticEngine()
+            try hapticEngine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func simpleSuccess() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
     }
 }
 
