bf8e1740bae0740c943167d06202a1adb934de3e
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-09-01 10:37:30 -0400

    Day 86/Flashzilla: App boilerplate

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Christopher Sherman on 2023-09-01.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
