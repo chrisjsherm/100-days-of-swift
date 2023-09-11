d381707351113ac6b62aa91ac3645ce161dc8d3b
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-09-11 08:56:51 -0400

    Day 92/Use alignmentGuide modifier to change an alignment.

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+//        SwiftUI provides us with the alignmentGuide() modifier for just this purpose. This takes two parameters: the guide we want to change, and a closure that returns a new alignment. The closure is given a ViewDimensions object that contains the width and height of its view, along with the ability to read its various edges.
+        VStack(alignment: .leading) {
             Text("Hello, world!")
+                .alignmentGuide(.leading) { d in
+                    return d[.trailing]
+                }
+            Text("This is a longer line of text")
         }
-        .padding()
+        .background(.red)
+        .frame(width: 400, height: 400)
+        .background(.blue)
     }
 }
 
