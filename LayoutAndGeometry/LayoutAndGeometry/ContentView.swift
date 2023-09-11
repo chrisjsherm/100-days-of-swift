eeff62482342b3495d819c29ee01d3be515a0f81
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-09-11 09:06:49 -0400

    Day 92/Custom alignment guide

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,17 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        SwiftUI provides us with the alignmentGuide() modifier for just this purpose. This takes two parameters: the guide we want to change, and a closure that returns a new alignment. The closure is given a ViewDimensions object that contains the width and height of its view, along with the ability to read its various edges.
-        VStack(alignment: .leading) {
-            Text("Hello, world!")
-                .alignmentGuide(.leading) { d in
-                    return d[.trailing]
-                }
-            Text("This is a longer line of text")
+        // Align the two views with midAccountAndName so their centers are
+        // both on the midAccountAndName guide of HStack.
+        HStack(alignment: .midAccountAndName) {
+            VStack {
+                Text("@twostraws")
+                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+                Image("paul-hudson")
+                    .resizable()
+                    .frame(width: 64, height: 64)
+            }
+
+            VStack {
+                Text("Full name:")
+                Text("PAUL HUDSON")
+                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+                    .font(.largeTitle)
+            }
         }
-        .background(.red)
-        .frame(width: 400, height: 400)
-        .background(.blue)
     }
 }
 
---
+++ LayoutAndGeometry/LayoutAndGeometry/Extension-VerticalAlignment.swift
@@ -0,0 +1,18 @@
+//
+//  VerticalAlignment.swift
+//  LayoutAndGeometry
+//
+//  Created by Christopher Sherman on 2023-09-11.
+//
+
+import SwiftUI
+
+extension VerticalAlignment {
+    enum MidAccountAndName: AlignmentID {
+        static func defaultValue(in d: ViewDimensions) -> CGFloat {
+            d[.top]
+        }
+    }
+
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+}
