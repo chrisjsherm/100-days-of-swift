ef85432583f9b647abde1f54c0f56eacc5d47e23
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-09-11 08:29:40 -0400

    Day 92/App boilerplate

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Christopher Sherman on 2023-09-11.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
