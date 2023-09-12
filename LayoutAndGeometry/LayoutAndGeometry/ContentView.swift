dd217603c6282881a98f7ed2bf41d2377883b850
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-09-12 08:38:40 -0400

    Day 93/Use GeometryReader to get global, custom, and local container sizes

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -1,30 +1,39 @@
-//
-//  ContentView.swift
-//  LayoutAndGeometry
-//
-//  Created by Christopher Sherman on 2023-09-11.
-//
-
 import SwiftUI
 
-struct ContentView: View {
+struct OuterView: View {
     var body: some View {
         VStack {
-            GeometryReader { geo in
-                Text("Hello, World!")
-                    .frame(width: geo.size.width * 0.9, height: 40)
-                    .background(.red)
-            }
-            .background(.green)
-    
-            Text("More text")
-                .background(.blue)
+            Text("Top")
+            InnerView()
+                .background(.green)
+            Text("Bottom")
         }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct InnerView: View {
+    var body: some View {
+        HStack {
+            Text("Left")
+            GeometryReader { geo in
+                Text("Center")
+                    .background(.blue)
+                    .onTapGesture {
+                        print("Global center: \(geo.frame(in: .global).midX) x \(geo.frame(in: .global).midY)")
+                        print("Custom center: \(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midY)")
+                        print("Local center: \(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midY)")
+                    }
+            }
+            .background(.orange)
+            Text("Right")
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        OuterView()
+            .background(.red)
+            .coordinateSpace(name: "Custom")
     }
 }
