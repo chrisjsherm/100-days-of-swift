1caa2a8b6ece789b5b02e6b16db19e0752ba47fc
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-09-14 08:49:32 -0400

    Day 94/Make colors change on scroll

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,18 +7,34 @@ struct ContentView: View {
         GeometryReader { fullView in
             ScrollView(.vertical) {
                 ForEach(0..<50) { index in
-                    GeometryReader { geo in
+                        GeometryReader { geo in
+                        
                         Text("Row #\(index)")
                             .font(.title)
                             .frame(maxWidth: .infinity)
-                            .background(colors[index % 7])
+                            .background(colors[abs(
+                                Int(
+                                    geo.frame(in: .global).minY / geo.size.height - 1.0
+                                ) % 7)
+                            ])
                             .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
                             .opacity(geo.frame(in: .global).maxY / fullView.size.height)
                             .scaleEffect(
                                 geo.frame(in: .global).maxY / fullView.size.height + 0.5
                             )
+                            .onTapGesture {
+                                print("Item height: \(geo.size.height)")
+                                print("Index \(index) maxY: \(geo.frame(in: .global).maxY)")
+                                print("View height: \(fullView.size.height)")
+                                let colorIdx = Int(
+                                    geo.frame(in: .global).minY / geo.size.height - 1.0
+                                ) % 7
+                                
+                                print("Color index: \(colorIdx)")
+                            }
                     }
                     .frame(height: 40)
+                    
                 }
             }
         }
