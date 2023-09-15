3f1242f458eae806b03f20f7d4a9bae02fcdf3da
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-09-15 13:30:30 -0400

    Day 96/Snow Seeker boilerplate

---
+++ Snow Seeker/Snow Seeker/Snow_SeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snow_SeekerApp.swift
+//  Snow Seeker
+//
+//  Created by Christopher Sherman on 2023-09-15.
+//
+
+import SwiftUI
+
+@main
+struct Snow_SeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
