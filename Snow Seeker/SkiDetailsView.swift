6410d7abfe822bdb30c331493189a9a0528efd4a
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2023-09-19 09:32:25 -0400

    Snow Seeker/Resort and Ski Details views

---
+++ Snow Seeker/SkiDetailsView.swift
@@ -0,0 +1,37 @@
+//
+//  SkiDetailsView.swift
+//  Snow Seeker
+//
+//  Created by Christopher Sherman on 2023-09-19.
+//
+
+import SwiftUI
+
+struct SkiDetailsView: View {
+    let resort: Resort
+
+    var body: some View {
+        Group {
+            VStack {
+                Text("Elevation")
+                    .font(.caption.bold())
+                Text("\(resort.elevation)m")
+                    .font(.title3)
+            }
+
+            VStack {
+                Text("Snow")
+                    .font(.caption.bold())
+                Text("\(resort.snowDepth)cm")
+                    .font(.title3)
+            }
+        }
+        .frame(maxWidth: .infinity)
+    }
+}
+
+struct SkiDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SkiDetailsView(resort: Resort.example)
+    }
+}
